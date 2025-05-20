c1321526b64fd7449f7818c264064ac399555499
Kai-X-Org <k.px@berkeley.edu>
2025-05-19 17:56:11 -0700

    set zoo to feedback minimum cum reward across the two agents

--- src/scenic/zoo/envs/scenic_zoo.py
+++ src/scenic/zoo/envs/scenic_zoo.py
@@ -89,13 +89,16 @@ class ScenicZooEnv(ParallelEnv):
                             cum_rew_0 = self.cumulative_rewards['agent0']
                             cum_rew_1 = self.cumulative_rewards['agent1']
 
-                            self.feedback_result = min(-np.max(np.array(result.records['ego_drift'])[:, 1]) - cum_rew_0,
-                                                       -np.max(np.array(result.records['car2_drift'])[:, 1]) - cum_rew_1,)
+                            self.feedback_result = min(cum_rew_0, cum_rew_1)
+
+                            # self.feedback_result = min(-np.max(np.array(result.records['ego_drift'])[:, 1]) - cum_rew_0,
+                                                       # -np.max(np.array(result.records['car2_drift'])[:, 1]) - cum_rew_1,)
                             # Note: the yield statement really is the last part to be executed in this run
                             # in an episode. Shouldn't put code after it
                             # print("SETTING MAX DEV")
                             self.max_deviation = max(np.max(np.array(result.records['ego_drift'])[:, 1]),
                                                        np.max(np.array(result.records['car2_drift'])[:, 1]))
+
                             actions = yield observation, reward, done(), truncated(), info 
 
                             break # a little unclean right here
