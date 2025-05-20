b9f3c6aff80bf7443df3dbf9c34148438a805060
Kai-X-Org <k.px@berkeley.edu>
2025-05-19 20:27:05 -0700

    ready to eval

--- src/scenic/simulators/metadrive/eval.py
+++ src/scenic/simulators/metadrive/eval.py
@@ -63,11 +63,11 @@ class Args:
     
     use_pretrained: bool = True
     
-    checkpoint_model: str = "models/uniform_deviation_baseline_05_19_02_33.pth"
+    checkpoint_model: str = "models/ce_reward_feedback_05_19_17_59.pth"
 
     eval_results_folder: str = "eval_results"
 
-    eval_result_file_name: str = "uniform_deviation_baseline_05_19_02_33"
+    eval_result_file_name: str = "ce_reward_feedback_05_19_17_59"
 
 
 LOG_STD_MAX = 2
