dd8754f9a7b833f20ee475043e17e363aab5ae2d
Kai-X-Org <k.px@berkeley.edu>
2025-06-19 23:13:10 -0700

    stuff seems to not work

--- src/scenic/simulators/habitat/ray_train.py
+++ src/scenic/simulators/habitat/ray_train.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
                       head_fcnet_hiddens=[256],
                   )
               )
-              .env_runners(num_env_runners=12,
+              .env_runners(num_env_runners=1,
                            gym_env_vectorize_mode=("ASYNC"))
 
             )
