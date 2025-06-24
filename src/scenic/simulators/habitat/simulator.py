84d4071e5f94214dfd34da417a22dc6ca2ef9378
Kai-X-Org <k.px@berkeley.edu>
2025-06-23 19:34:03 -0700

    seems like eval call back is killing training

--- src/scenic/simulators/habitat/simulator.py
+++ src/scenic/simulators/habitat/simulator.py
@@ -506,7 +506,7 @@ class HabitatSimulation(Simulation):
         # print("closed env")
         # super().destroy()
         # return
-        make_vid = True
+        make_vid = False
         if make_vid:
             folder_name = "test_run_vids/"
 
